4caa5a5febacc012aea935db6cefc219cccde694
Jhonatan Hoyos Marina <jhoyosmarina@gmail.com>
2021-07-03 11:14:11 -0500

    aa

--- config/settings.js
+++ config/settings.js
@@ -1,3 +1,3 @@
 export const apiBaseUrl = process.env.REACT_APP_API_URL;
 
-export const apilocalUrl = process.env.REACT_APP_LOCAL_URL;
+export const apilocalUrl = process.env.REACT_APP_LOCAL_URL || process.env.PORT;
