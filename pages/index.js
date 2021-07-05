5edf18cc533d74e2a3da59faa9a059efa1ec880f
Jhonatan Hoyos Marina <jhoyosmarina@gmail.com>
2021-07-05 15:45:11 -0500

    wasa 19

--- pages/index.js
+++ pages/index.js
@@ -23,7 +23,7 @@ class Index extends Component {
             width: "60px",
             height: "60px",
             bottom: "50px",
-            right: "1000px",
+            right: "1%",
             color: "#FFF",
             borderRadius: "50px",
             textAlign: "center",
