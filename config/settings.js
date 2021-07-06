85b5cbf04a8f0c3330e9298dce5273bea2eaa740
Jhonatan Hoyos Marina <jhoyosmarina@gmail.com>
2021-07-06 10:32:38 -0500

    webF 14

--- config/settings.js
+++ config/settings.js
@@ -1,3 +1,5 @@
-export const apiBaseUrl = process.env.REACT_APP_API_PROD;
+export const apiBaseUrl =
+  process.env.REACT_APP_API_PROD || "http://localhost:4000";
 
-export const apilocalUrl = process.env.REACT_APP_POR_URL;
+export const apilocalUrl =
+  process.env.REACT_APP_POR_URL || "http://localhost:4000";
