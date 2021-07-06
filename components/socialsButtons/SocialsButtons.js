47d5d90d3708126670efb8a1a81ea5ba4e7ca2e6
Jhonatan Hoyos Marina <jhoyosmarina@gmail.com>
2021-07-06 12:37:26 -0500

    webF 23

--- components/socialsButtons/SocialsButtons.js
+++ components/socialsButtons/SocialsButtons.js
@@ -4,13 +4,13 @@ import WhatsAppWidget from "react-whatsapp-widget";
 import "react-whatsapp-widget/dist/index.css";
 const SocialsButtons = () => {
   return (
-    <section className="wasa pt-100" id="about">
-      <div>
+    <>
+      <div className="wasa">
         <WhatsAppWidget phoneNumber="51937403683" />
       </div>
 
       <MessengerCustomerChat pageId="114975533587822" appId="222335366276258" />
-    </section>
+    </>
   );
 };
 
